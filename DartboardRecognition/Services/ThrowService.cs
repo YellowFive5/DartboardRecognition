000fb0d6f4ff026bf9cfb322227c851510f7fcf8
YellowFive5 <yellowfive555@yahoo.com>
2019-10-19 20:15:26 +0300

    ThrowService loop pretty refactor

--- DartboardRecognition/Services/ThrowService.cs
+++ DartboardRecognition/Services/ThrowService.cs
@@ -40,14 +40,21 @@ namespace DartboardRecognition.Services
             var anotherThrow = PrepareThrowData(poi);
             throwsCollection.Enqueue(anotherThrow);
 
+            drawService.ProjectionDrawLine(firstBestRay.CamPoint, firstBestRay.RayPoint, true);
+            drawService.ProjectionDrawLine(secondBestRay.CamPoint, secondBestRay.RayPoint, false);
+            drawService.ProjectionDrawThrow(poi, false);
             drawService.PrintThrow(anotherThrow);
-            drawService.DrawThrow(poi);
         }
 
         private Throw PrepareThrowData(PointF poi)
         {
-            var startRadSector = -1.41372;
-            var radSectorStep = 0.314159;
+            var sectors = new List<int>()
+                          {
+                              14, 9, 12, 5, 20,
+                              1, 18, 4, 13, 6,
+                              10, 15, 2, 17, 3,
+                              19, 7, 16, 8, 11
+                          };
             var angle = MeasureService.FindAngle(drawService.ProjectionCenterPoint, poi);
             var distance = MeasureService.FindDistance(drawService.ProjectionCenterPoint, poi);
             var sector = 0;
@@ -58,110 +65,43 @@ namespace DartboardRecognition.Services
             {
                 multiplier = 3;
             }
-
-            if (distance >= drawService.ProjectionCoefficent * 160 &&
-                distance <= drawService.ProjectionCoefficent * 170)
+            else if (distance >= drawService.ProjectionCoefficent * 160 &&
+                     distance <= drawService.ProjectionCoefficent * 170)
             {
                 multiplier = 2;
             }
 
             // Find sector
-            if (angle >= -1.41372 && angle < -1.099561)
-            {
-                sector = 1;
-            }
-            else if (angle >= -1.099561 && angle < -0.785402)
-            {
-                sector = 18;
-            }
-            else if (angle >= -0.785402 && angle < -0.471243)
-            {
-                sector = 4;
-            }
-            else if (angle >= -0.471243 && angle < -0.157084)
-            {
-                sector = 13;
-            }
-            else if (angle >= -0.157084 && angle < 0.157075)
-            {
-                sector = 6;
-            }
-            else if (angle >= 0.157075 && angle < 0.471234)
-            {
-                sector = 10;
-            }
-            else if (angle >= 0.471234 && angle < 0.785393)
-            {
-                sector = 15;
-            }
-            else if (angle >= 0.785393 && angle < 1.099552)
-            {
-                sector = 2;
-            }
-            else if (angle >= 1.099552 && angle < 1.413711)
-            {
-                sector = 17;
-            }
-            else if (angle >= 1.413711 && angle < 1.72787)
-            {
-                sector = 3;
-            }
-            else if (angle >= 1.72787 && angle < 2.042029)
-            {
-                sector = 19;
-            }
-            else if (angle >= 2.042029 && angle < 2.356188)
-            {
-                sector = 7;
-            }
-            else if (angle >= 2.356188 && angle < 2.670347)
-            {
-                sector = 16;
-            }
-            else if (angle >= 2.670347 && angle < 2.984506)
-            {
-                sector = 8;
-            }
-            else if (angle >= 2.984506 && angle < 3.14159 ||
-                     angle >= -3.14159 && angle < -3.29868)
-            {
-                sector = 11;
-            }
-            else if (angle >= -3.29868 && angle < -2.67036)
-            {
-                sector = 14;
-            }
-            else if (angle >= -2.67036 && angle < -2.3562)
-            {
-                sector = 9;
-            }
-            else if (angle >= -2.3562 && angle < -2.04204)
-            {
-                sector = 12;
-            }
-            else if (angle >= -2.04204 && angle < -1.72788)
-            {
-                sector = 5;
-            }
-            else if (angle >= -1.72788 && angle < -1.41372)
-            {
-                sector = 20;
-            }
-
             if (distance <= drawService.ProjectionCoefficent * 7)
             {
                 sector = 50;
             }
-
-            if (distance > drawService.ProjectionCoefficent * 7 &&
-                distance <= drawService.ProjectionCoefficent * 17)
+            else if (distance > drawService.ProjectionCoefficent * 7 &&
+                     distance <= drawService.ProjectionCoefficent * 17)
             {
                 sector = 25;
             }
-
-            if (distance > drawService.ProjectionCoefficent * 170)
+            else if (distance > drawService.ProjectionCoefficent * 170)
             {
                 sector = 0;
+            }
+            else
+            {
+                var startRadSector = -2.9845105;
+                var radSectorStep = 0.314159;
+                var radSector = startRadSector;
+                foreach (var proceedSector in sectors)
+                {
+                    if (angle >= radSector && angle < radSector + radSectorStep)
+                    {
+                        sector = proceedSector;
+                        break;
+                    }
+
+                    sector = 11;
+
+                    radSector += radSectorStep;
+                }
             }
 
             return new Throw(poi, sector, multiplier, drawService.ProjectionFrameSide);
